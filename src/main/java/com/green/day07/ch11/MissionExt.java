90ac06495068ec7a9019bf1d0e04e4730cd1815b
김수지 <choyen2727@naver.com>
2024-10-16 17:23:53 +0900

    first

--- src/main/java/com/green/day07/ch11/MissionExt.java
+++ src/main/java/com/green/day07/ch11/MissionExt.java
@@ -3,7 +3,7 @@ package com.green.day07.ch11;
 public class MissionExt {
     public static void main(String[] args) {
         String fileNm="nice_Very.Nice.jpg";
-        int a=fileNm.lastIndexOf(".")+1;
+        int a=fileNm.lastIndexOf(".")+1;  //결과값이 return되어 저장된 것이므로 ++을 사용할 수 없다.
         String ext=fileNm.substring(a);
         System.out.println(ext);
 
